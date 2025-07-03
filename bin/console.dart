6c8f5480b485e206080debf0dde81b3d7d8ab410
Klas Kalass <klas.r.kalass@googlemail.com>
2025-07-03 22:10:29 +0200

    fetch all results

--- bin/console.dart
+++ bin/console.dart
@@ -6,10 +6,10 @@ void main(List<String> arguments) async {
   final client = PubClient();
 
   print('Searching for packages with analyzer dependency...');
-  
+
   // Fetch all pages of search results
   final allPackages = await fetchAllPackagesWithPagination(client);
-  
+
   print('Found ${allPackages.length} packages total across all pages');
 
   // Create a service instance to reuse across operations for better performance
@@ -99,7 +99,9 @@ Future<SearchResults> listPackages(PubClient client) async {
 ///
 /// Iterates through all pages of search results to get the complete list
 /// of packages that depend on the analyzer package.
-Future<List<PackageResult>> fetchAllPackagesWithPagination(PubClient client) async {
+Future<List<PackageResult>> fetchAllPackagesWithPagination(
+  PubClient client,
+) async {
   final allPackages = <PackageResult>[];
   SearchResults? currentResults;
   int pageCount = 0;
@@ -118,7 +120,9 @@ Future<List<PackageResult>> fetchAllPackagesWithPagination(PubClient client) asy
       allPackages.addAll(currentResults.packages);
       pageCount++;
 
-      print('Page $pageCount: Found ${currentResults.packages.length} packages (total: ${allPackages.length})');
+      print(
+        'Page $pageCount: Found ${currentResults.packages.length} packages (total: ${allPackages.length})',
+      );
 
       // Small delay between page requests to be respectful to the API
       await Future.delayed(const Duration(milliseconds: 200));
@@ -128,6 +132,8 @@ Future<List<PackageResult>> fetchAllPackagesWithPagination(PubClient client) asy
     }
   }
 
-  print('Completed pagination: $pageCount pages, ${allPackages.length} total packages');
+  print(
+    'Completed pagination: $pageCount pages, ${allPackages.length} total packages',
+  );
   return allPackages;
 }
